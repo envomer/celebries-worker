e84b5de5ec0763998d4e54eba49b64c47986954a
Omer Mohamed Ali <envomer@users.noreply.github.com>
2022-11-11 01:16:29 +0400

    Remove path from logging

--- logger/logging.go
+++ logger/logging.go
@@ -5,23 +5,23 @@ import "github.com/evolidev/evoli/framework/logging"
 var AppLog = logging.NewLogger(&logging.Config{
 	Name:        "app",
 	PrefixColor: 165,
-	Path:        "log.log",
+	//Path:        "log.log",
 })
 
 var WebLog = logging.NewLogger(&logging.Config{
 	Name:        "web",
 	PrefixColor: 169,
-	Path:        "log.log",
+	//Path:        "log.log",
 })
 
 var VersionLog = logging.NewLogger(&logging.Config{
 	Name:        "version",
 	PrefixColor: 145,
-	Path:        "log.log",
+	//Path:        "log.log",
 })
 
 var CronLog = logging.NewLogger(&logging.Config{
 	Name:        "cron",
 	PrefixColor: 135,
-	Path:        "cron.log",
+	//Path:        "cron.log",
 })
